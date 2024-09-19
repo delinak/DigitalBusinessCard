43d00194f93aae4ffe7172d31210fbd75198f042
delinak <delina.k14@gmail.com>
2024-09-19 13:38:04 -0700

    removed files.

---
+++ src/buttons.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+
+const email = () => {
+    window.location.href="mailto:delina.k14@gmail.com";
+};
+
+const linkedin = () => {
+    window.location.href="https://www.linkedin.com/in/delinak/";
+};
+
+export { email, linkedin};
